195923f0d9ab3bd054c91f112c3ce93ddd98fe64
faizalkhan <faizalkhan.s@gmail.com>
2025-11-07 13:00:24 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const app = express();
 
 const allowedOrigins = [
   "https://www.happysystems.in",
+  "https://happie-systems.vercel.app",
   "http://localhost:3001",
 ];
 
@@ -29,6 +30,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
      allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+     optionsSuccessStatus: 200 
   })
 );
 
